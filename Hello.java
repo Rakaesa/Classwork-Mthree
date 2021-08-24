a83aa682f66614fea432ef272d7df2e827f8f321
Rakaesa <54375203+Rakaesa@users.noreply.github.com>
2021-08-24 16:42:41 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/**
+ *@author Your Name
+ *email: cdoonan0428@gmail.com
+ *date: 08/24/2021
+ *purpose: NOT my first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+public class Hello {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Hello, Connor!");
+    }
+    
+}
